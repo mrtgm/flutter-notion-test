9fc415883bb17eaaa4e6519b67be3250b263b399
Morio <xtarako@gmail.com>
2022-05-28 17:21:55 +0900

    add tab

--- lib/screens/article_screen.dart
+++ lib/screens/article_screen.dart
@@ -37,51 +37,75 @@ class _ArticleScreenState extends State<ArticleScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(widget.title)),
-      body: FutureBuilder(
-          future: _article,
-          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-            if (!snapshot.hasData) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            final article = snapshot.data!;
-            return Padding(
-              padding: const EdgeInsets.all(24.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        widget.title,
-                        style: TextStyle(fontSize: 24.0),
-                      ),
-                      Text(widget.client),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 24.0,
-                  ),
-                  Expanded(
-                    child: Markdown(
-                      data: article,
-                      imageBuilder: (uri, title, alt) {
-                        return Center(
-                          child: ExtendedImage.network(
-                            uri.toString(),
-                            height: ScreenUtil().setHeight(300),
-                            cache: true,
+      body: DefaultTabController(
+        length: 2,
+        child: FutureBuilder(
+            future: _article,
+            builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+              if (!snapshot.hasData) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              final article = snapshot.data!;
+              return Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                widget.title,
+                                style: TextStyle(fontSize: 24.0),
+                              ),
+                              Text(widget.client),
+                            ],
                           ),
-                        );
-                      },
+                        ),
+                        SizedBox(
+                          width: 24.0,
+                        ),
+                        Icon(Icons.star_border, size: 32.0)
+                      ],
                     ),
-                  ),
-                ],
-              ),
-            );
-          }),
+                    SizedBox(
+                      height: 24.0,
+                    ),
+                    TabBar(
+                      labelColor: Colors.black,
+                      tabs: [
+                        Tab(text: "Content"),
+                        Tab(text: "Flow"),
+                      ],
+                    ),
+                    Expanded(
+                      child: TabBarView(children: [
+                        Markdown(
+                          data: article,
+                          imageBuilder: (uri, title, alt) {
+                            return Center(
+                              child: ExtendedImage.network(
+                                uri.toString(),
+                                height: ScreenUtil().setHeight(300),
+                                cache: true,
+                              ),
+                            );
+                          },
+                        ),
+                        Text("flow"),
+                      ]),
+                    ),
+                  ],
+                ),
+              );
+            }),
+      ),
     );
   }
 }
